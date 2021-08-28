8271ce523ac77fc63dd49dcefb1db9323aabfca5
Vikas Singh <ic3fir3wat3r@gmail.com>
2021-08-28 16:53:16 +0530

    Enemy Script created with Inheritance, ScipatableObjects and Finite State Machine. So we can add different types of Enemies with different behaviour with different States (move, idle, attack, jump and so on).

---
+++ Assets/Scripts/Enemy/EnemySpecific/Enemy1/E1_IdleState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E1_IdleState : IdleState
+{
+    private Enemy1 enemy;
+    public E1_IdleState(Entity _entity, FiniteStateMachine _stateMachine, string _animBoolName, D_IdleState _stateData, Enemy1 _enemy) : base(_entity, _stateMachine, _animBoolName, _stateData)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate(); 
+        
+        if (isIdleTimeOver)
+        {
+            stateMachine.ChangeState(enemy.moveState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
---
+++ Assets/Scripts/Enemy/States/IdleState.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class IdleState : State
+{
+    protected D_IdleState stateData;
+
+    protected bool flipAfterIdle;
+    protected bool isIdleTimeOver;
+
+    protected float idleTime;
+    public IdleState(Entity _entity, FiniteStateMachine _stateMachine, string _animBoolName, D_IdleState _stateData) : base(_entity, _stateMachine, _animBoolName)
+    {
+        this.stateData = _stateData;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        entity.SetVelocity(0f);
+        isIdleTimeOver = false;
+        SetRandomIdleTime();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        if (flipAfterIdle)
+        {
+            entity.Flip();
+        }
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (Time.time >= startTime + idleTime)
+        {
+            isIdleTimeOver = true;
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public void SetFlipAfterIdle(bool _flip)            // Setter for flipAfterIdle
+    {
+        flipAfterIdle = _flip;
+    }
+
+    private void SetRandomIdleTime()
+    {
+        idleTime = Random.Range(stateData.minIdleTime, stateData.maxIdleTime);
+    }
+}
